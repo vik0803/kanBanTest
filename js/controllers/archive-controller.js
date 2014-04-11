e593250ade84cd2eb19354963532c6bd49ffad7f
Alexander Kundurdjiev <alexander.a.kundurdjiev@gmail.com>
2014-04-11 17:23:17 +0300

    remove archive-controller.js

--- js/controllers/archive-controller.js
+++ js/controllers/archive-controller.js
@@ -3,11 +3,4 @@
 angular.module('project').
 	controller('ArchiveCtrl', ['$scope', 'ticketsXhr','$log',function ($scope,ticketsXhr,$log) {
 		$scope.loading = true;
-		// ticketsXhr.archiveTickets().then( function ( response ) {
-		// 	console.log('msg')
-		//     $scope.loading = false;
-		// }, function ( response ) {
-		//     // TODO: handle the error somehow
-		//     $scope.loading = false;
-		// });
 	}]);
